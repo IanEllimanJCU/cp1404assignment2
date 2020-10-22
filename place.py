9f64153c4e553118b8da675bed396da29a80f55e
IanEllimanJCU <ian.elliman@my.jcu.edu.au>
2020-10-22 14:47:53 +1000

    Initial commit

---
+++ place.py
@@ -0,0 +1,12 @@
+def __init__
+
+def __str__
+
+def mark_place_visited
+
+def mark_place_unvisited
+
+def check_if_place_important
+    if self.priority <= 2
+        return self.important = True
+
---
+++ placecollection.py
@@ -0,0 +1,9 @@
+def load_places
+
+def save_places
+
+def add_place
+
+def get_number_unvisited_places
+
+def sort
